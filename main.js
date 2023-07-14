1409ac826ed6890bcc647bdc07c7e1d0a09a5727
f-Rodeh <rodeh.dev@gmail.com>
2023-07-14 16:43:21 -0600

    Add function to display new books

--- main.js
+++ main.js
@@ -7,6 +7,37 @@ function Book(title, author, pagesCount, readByUser) {
   this.readByUser  = readByUser;
 }
 
+Book.prototype.display = function (){
+  const card = document.createElement('div');
+  card.classList.add('card');
+  cardContainer.appendChild(card);
+
+  addElementToCard(card, 'h1', this.title);
+  addElementToCard(card, 'h2', this.author);
+  addElementToCard(card, 'span', this.pagesCount + ' pages');
+  addElementToCard(card, 'div')
+    .classList.add('read-status');
+}
+
+let hungerGames = new Book('The hunger games', 'Suzzane Collins', 384, true)
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+const cardContainer = document.querySelector('main');
+
+function displayLibrary() {
+  for (const book of myLibrary) {
+    book.display();
+  }
+}
+
+function addElementToCard(parent, type, content){
+  const element = document.createElement(type);
+  if( content ){
+    element.textContent = content;
+  }
+  parent.appendChild(element);
+  return element;
 }
